0289028e465c814d3e4af3e60d154aa9df3f289d
Brea Gaudioso <breagaudioso@gmail.com>
2015-10-20 09:36:33 -0700

    merge conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   def index
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     puts '8888888888888888888888888888888888888888'
-    puts hash = spotify_user.to_hash
+    hash = spotify_user.to_hash
   end
-
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root 'sessions#login'
   get 'login', to: 'sessions#login', as: 'login'
   get 'auth/spotify/callback', to: 'users#index'
