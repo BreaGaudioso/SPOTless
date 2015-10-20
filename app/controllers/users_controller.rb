9e397f4d99b2ed74944d1d241710118888d244dd
Donald Brunais <donb1991@hotmail.com>
2015-10-20 09:37:37 -0700

    fixed log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,23 +7,21 @@ class UsersController < ApplicationController
 
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    binding.pry
     hashToken = spotify_user.to_hash["credentials"]["token"]
     hashID = spotify_user.to_hash["id"]
     if hashID.present? && hashToken.present?
       found_user = User.where(spotify_user_id:hashID).first
-      binding.pry
       if found_user
         session[:user_id] = found_user["id"]
         redirect_to users_path
-      end
-    else
-      user = User.create( spotify_user_id:hashID, spotify_auth_token:hashToken )
-      if user.save
-        session[:user_id] = user.id
-        redirect_to users_path
       else
-        redirect_to users_path
+        user = User.create( spotify_user_id:hashID, spotify_auth_token:hashToken )
+        if user.save
+          session[:user_id] = user.id
+          redirect_to users_path
+        else
+          redirect_to users_path
+        end
       end
     end
   end
