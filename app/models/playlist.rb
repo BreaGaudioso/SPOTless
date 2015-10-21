cacd42355ee7776dd1779ed0e8d541b907f828ac
Donald Brunais <donb1991@hotmail.com>
2015-10-21 16:25:33 -0700

    add has_dup method

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,4 +4,12 @@ class Playlist < ActiveRecord::Base
   has_many :playlist_tracks
   has_many :tracks, through: :playlist_tracks
 
+  def has_dup
+    self.playlist_tracks do |playlist_track|
+      if playlist_track.copies > 0
+        return true
+      end
+    end
+    false
+  end
 end
