72e09b42bc2886a0df10b28abf5336a33867d799
Donald Brunais <donb1991@hotmail.com>
2015-10-22 10:27:49 -0700

    move logic to worker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  def loading
+
+  end
+
   def current_user
     return unless session[:user_id]
     @current_user ||=User.find session[:user_id]
