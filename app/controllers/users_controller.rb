441d97754fe14ca9e663efde53a7cfbe4670d249
Teagan Wrest <wrestteagan@gmail.com>
2015-10-23 09:20:23 -0700

    styling changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,15 @@ class UsersController < ApplicationController
     hashToken = spotify_user.to_hash["credentials"]["token"]
     hashID = spotify_user.to_hash["id"]
     binding.pry
+    if spotify_user.display_name.present?
+      displayName = spotify_user.display_name.present?
+    else
+      displayName = spotify_user.id
+    end
     #makes sure we have a vaild user back from spotify
     if hashID.present? && hashToken.present?
       #finds or creates a user by spotify id
-      found_user = User.where(spotify_user_id:hashID, spotify_user_id:spotify_user.id).first_or_create
+      found_user = User.where(spotify_user_id:hashID, spotify_user_id:spotify_user.id, username:displayName).first_or_create
       found_user.update_attributes(spotify_auth_token:hashToken)
       if found_user
         #logs user in
