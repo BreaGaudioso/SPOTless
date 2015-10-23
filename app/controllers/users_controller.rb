8657205763f4e21b1bcaf1c2dfd67c1f70e43f4f
Donald Brunais <donb1991@hotmail.com>
2015-10-23 15:44:17 -0700

    fixed update user bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,14 +12,13 @@ class UsersController < ApplicationController
 
 
   def spotify
+    found_user = User.where(spotify_user_id:request.env['omniauth.auth'].info.id).first_or_create
     if request.env['omniauth.auth'].info.display_name.present?
       userName = request.env['omniauth.auth'].info.display_name
     else
       userName = request.env['omniauth.auth'].info.id
     end
     found_user.update_attributes(spotify_auth_token:request.env['omniauth.auth'].credentials.token, username:userName)
-    found_user = User.where(spotify_user_id:request.env['omniauth.auth'].info.id).first_or_create
-    found_user.update_attributes(spotify_auth_token:request.env['omniauth.auth'].credentials.token)
     PlaylistRequest.perform_async(found_user.id)
     session[:user_id] = found_user.id
     flash[:sucess] = 'Signed In'
