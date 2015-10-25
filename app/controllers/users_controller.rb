9c708e29cb401f4babfa24b6bae920c6e17561d3
Donald Brunais <donb1991@hotmail.com>
2015-10-25 16:04:36 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,15 @@ class UsersController < ApplicationController
 
   def spotify
     found_user = User.where(spotify_user_id:request.env['omniauth.auth'].info.id).first_or_create
-    if request.env['omniauth.auth'].info.display_name.present?
-      userName = request.env['omniauth.auth'].info.display_name
-    else
-      userName = request.env['omniauth.auth'].info.id
-    end
-    found_user.update_attributes(spotify_auth_token:request.env['omniauth.auth'].credentials.token, username:userName)
-    # PlaylistRequest.perform_async(found_user.id)
-    session[:user_id] = found_user.id
-    flash[:sucsess] = 'Signed In'
+    # if request.env['omniauth.auth'].info.display_name.present?
+    #   userName = request.env['omniauth.auth'].info.display_name
+    # else
+    #   userName = request.env['omniauth.auth'].info.id
+    # end
+    # found_user.update_attributes(spotify_auth_token:request.env['omniauth.auth'].credentials.token, username:userName)
+    # # PlaylistRequest.perform_async(found_user.id)
+    # session[:user_id] = found_user.id
+    # flash[:sucsess] = 'Signed In'
     redirect_to user_path(found_user)
   end
 
